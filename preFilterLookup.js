eefd19d12c6be898ba87cab964fd18c4b0c9a0ff
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-09 17:05:51 -0700

    commiting changes to the script to apply it to more than one form

--- preFilterLookup.js
+++ preFilterLookup.js
@@ -1,17 +1,25 @@
-function preFilterLookup()
+function preFilterLookup(fieldName, clientName, fieldDepartment)
 {
-	Xrm.Page.getControl("ll_hapgoal").addPreSearch(function () {
-      addLookupFilter();
+	Xrm.Page.getControl(fieldName).addPreSearch(function () {
+      addLookupFilter(fieldName, clientName, fieldDepartment);
    });
 } 
 
-function addLookupFilter()
+function addLookupFilter(fieldName, clientName, fieldDepartment)
 {
-    var department = Xrm.Page.getAttribute("ll_department").getValue();
-	var client = Xrm.Page.getAttribute("ll_clientnameid").getValue();
+	var entityName = Xrm.Page.data.entity.getEntityName();
+                     var client = Xrm.Page.getAttribute(clientName).getValue();
+                     var department = Xrm.Page.getAttribute(fieldDepartment).getValue();
+	
 	 
-    if (department != null && client != null) {
+    if (department != null && client != null)  {
+	if (entityName == "ll_serviceprovided"){
         fetchXml = "<filter type='and'><condition attribute='ll_department' operator='eq' value='" + department[0].id + "' /><condition attribute='llaa_contactid' operator='eq' value='" + client[0].id + "' /></filter>";           
 		Xrm.Page.getControl("ll_hapgoal").addCustomFilter(fetchXml);
      }
+	 else if (entityName == "llaa_hapgoal" ) {
+        fetchXml = "<filter type='and'><condition attribute='ll_department' operator='eq' value='" + department[0].id + "' /><condition attribute='ll_clientnameid' operator='eq' value='" + client[0].id + "' /></filter>";           
+		Xrm.Page.getControl(fieldName).addCustomFilter(fetchXml);
+     }
+	 }
 }
