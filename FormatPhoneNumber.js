df0ca92b2c13bfe606f21c9c546bb36dbd318169
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-17 15:39:46 -0700

    updating the PhoneFormat script

--- FormatPhoneNumber.js
+++ FormatPhoneNumber.js
@@ -4,15 +4,22 @@ function FormatPhoneNumber(context)
 		var phoneNumber = context.getEventSource().getValue();
 		if (phoneNumber != null) {
 			var scrubbed = phoneNumber.toString().replace(/[^0-9]/g, "");
-			var sevenDigitFormat = /^\(?([0-9]{3})[-. ]?([0-9]{4})$/;
+			//var sevenDigitFormat = /^\(?([0-9]{3})[-. ]?([0-9]{4})$/;
 			var tenDigitFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 			var extDigitFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})?([0-9]*)$/;
-			if (tenDigitFormat.test(scrubbed)) {
+			
+			if (scrubbed.length < 10) {
+		       alert("Please enter a phone number including area code");
+			}
+			else if (scrubbed.length > 14) {
+		       alert("Phone number is too long. Please enter a valid phone number");
+			   }
+			else if (tenDigitFormat.test(scrubbed)) {
 				phoneNumber = scrubbed.replace(tenDigitFormat, "($1) $2-$3");
 			}
-			else if (sevenDigitFormat.test(scrubbed)) {
-				phoneNumber = scrubbed.replace(sevenDigitFormat, "$1-$2");
-			}
+			//else if (sevenDigitFormat.test(scrubbed)) {
+			//	phoneNumber = scrubbed.replace(sevenDigitFormat, "$1-$2");
+			//}
 			else if (extDigitFormat.test(scrubbed)) {
 				phoneNumber = scrubbed.replace(extDigitFormat, "($1) $2-$3 x$4");
 			}
