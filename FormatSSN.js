51543df5f3a53549754ee905cea19a29061fc7a9
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-17 17:11:25 -0700

    changed the FormatSSN script to setNotification instead of the alert. The benefits of setNotification is that it is blocking. So no need to add validation for the OnSave Event

--- FormatSSN.js
+++ FormatSSN.js
@@ -1,20 +1,29 @@
 function FormatSSN(context)
 {
-	if( ! IsNull(context)) {
-		var ssn = context.getEventSource().getValue();
-		if (ssn != null) {
+if( !IsNull(context)) {
+		var ssnField = context.getEventSource()
+		var ssn = ssnField.getValue();
+	if (ssn != null) {
 			var scrubbed = ssn.toString().replace(/[^0-9]/g, "");
 			var ssnFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{4})$/;
 			
-			
-			if (scrubbed.length !=9) {
-		       alert("Invalid SSN Number");
-			}
+		ssnField.controls.forEach(function (control) {
+			if (scrubbed.length != 9) {
+			control.setNotification("Enter a Valid Social Security Number");
+		} 
 			else if (ssnFormat.test(scrubbed)) {
-				ssn = scrubbed.replace(ssnFormat, "$1-$2-$3");
-			}
-		
-			context.getEventSource().setValue(ssn);
-		}
+			ssn = scrubbed.replace(ssnFormat, "$1-$2-$3"); 
+			control.clearNotification();
+                 }
+         });		
 	}
-}	
+	else {
+		ssnField.controls.forEach(function (control) {
+			control.clearNotification();
+		});	
+	}
+}
+ssnField.setValue(ssn);		
+}		
+			
+	
