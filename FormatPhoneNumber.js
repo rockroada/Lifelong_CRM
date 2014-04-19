740bb55e31f3d50ce27ccfbfa1c86267a944915e
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-18 17:17:57 -0700

    changed the script to display Notification instead of the alert

--- FormatPhoneNumber.js
+++ FormatPhoneNumber.js
@@ -8,22 +8,27 @@ function FormatPhoneNumber(context)
 			var tenDigitFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 			var extDigitFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})?([0-9]*)$/;
 			
-			if (scrubbed.length < 10) {
-		       alert("Please enter a phone number including area code");
-			}
-			else if (scrubbed.length > 14) {
-		       alert("Phone number is too long. Please enter a valid phone number");
+			context.getEventSource().controls.forEach(function (control) {
+				if (scrubbed.length < 10) {
+		      control.setNotification("Please enter a phone number including area code");
+				}
+				else if (scrubbed.length > 14) {
+		       control.setNotification("Phone number is too long. Please enter a valid phone number");
 			   }
-			else if (tenDigitFormat.test(scrubbed)) {
+				else if (tenDigitFormat.test(scrubbed)) {
 				phoneNumber = scrubbed.replace(tenDigitFormat, "($1) $2-$3");
-			}
+				control.clearNotification();
+				}
 			//else if (sevenDigitFormat.test(scrubbed)) {
 			//	phoneNumber = scrubbed.replace(sevenDigitFormat, "$1-$2");
 			//}
-			else if (extDigitFormat.test(scrubbed)) {
+				else if (extDigitFormat.test(scrubbed)) {
 				phoneNumber = scrubbed.replace(extDigitFormat, "($1) $2-$3 x$4");
-			}
+				control.clearNotification();
+				}
 			context.getEventSource().setValue(phoneNumber);
-		}
+			
+		});
 	}
+}
 }	
